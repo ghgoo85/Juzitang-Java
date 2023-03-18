c0bdf9991f928329983c8851b5d7d6b3a97e7482
ghgoo85 <meiku_555@163.com>
2023-03-18 22:20:17 +0800

    add Main Method

--- com/jzt/Cat.java
+++ com/jzt/Cat.java
@@ -1,5 +1,8 @@
 package JavaPro_01.com.jzt;
 
 public class Cat {
-  
+  String name;
+  public void eat() {
+    System.out.println("eating");
+  }
 }
--- com/jzt/Test01.java
+++ com/jzt/Test01.java
@@ -1,5 +1,8 @@
 package JavaPro_01.com.jzt;
 
 public class Test01 {
-  
+  public static void main(String[] args) {
+    Cat cat = new Cat();
+    cat.eat();
+  }
 }
